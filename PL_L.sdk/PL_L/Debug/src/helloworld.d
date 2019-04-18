b4029828fc4dc4b763069b15bd8f884631fe6c3e
NaokiOsako <tottontouhu0@gmail.com>
2019-04-18 17:36:07 +0900

    create

---
+++ PL_L.sdk/PL_L/Debug/src/helloworld.d
@@ -0,0 +1,39 @@
+src/helloworld.o src/helloworld.o: ../src/helloworld.c ../src/platform.h \
+ ../src/platform_config.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/bspconfig.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/sleep.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xil_io.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xil_printf.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xpseudo_asm.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h
+
+../src/platform.h:
+
+../src/platform_config.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/bspconfig.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/sleep.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xil_io.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xil_printf.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xpseudo_asm.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xreg_cortexa9.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xpseudo_asm_gcc.h:
---
+++ PL_L.sdk/PL_L/Debug/src/platform.d
@@ -0,0 +1,16 @@
+src/platform.o src/platform.o: ../src/platform.c \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xparameters.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xparameters_ps.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xil_cache.h \
+ ../../PL_L_bsp/ps7_cortexa9_0/include/xil_types.h \
+ ../src/platform_config.h
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xparameters.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xparameters_ps.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xil_cache.h:
+
+../../PL_L_bsp/ps7_cortexa9_0/include/xil_types.h:
+
+../src/platform_config.h:
